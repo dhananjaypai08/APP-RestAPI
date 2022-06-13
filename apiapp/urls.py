7212ae51b5ac3742bdc9aeab5c6220bf7725f9ae
Dhananjay <dhananjay2002pai@gmail.com>
2022-06-14 05:03:14 +0530

    update delete

--- apiapp/urls.py
+++ apiapp/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from apiapp import views
 
 urlpatterns = [
-    path('', views.getData, name='getdata'),
-    path('add', views.addData, name='adddata'),
-    path('delete', views.deleteData, name='deletedata'),
+    path('', views.showPatterns, name='showpatterns'),
+    path('view/', views.getData, name='getdata'),
+    path('view/<str:name>/', views.getSpecificData, name='getspecificdata'),
+    path('update/<str:name>/', views.updateData, name='updatedata'),
+    path('add/', views.addData, name='adddata'),
+    path('delete/<str:name>/', views.deleteData, name='deletedata'),
 ]
--- apiapp/views.py
+++ apiapp/views.py
@@ -1,10 +1,25 @@
+from importlib.metadata import packages_distributions
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from apiapp import serializers
 from apiapp.models import Person
 from apiapp.serializers import PersonSerializer
 from django.contrib import messages
 # Create your views here.
+
+
+@api_view(['GET'])
+def showPatterns(request):
+    urlpatterns = {  
+    '': 'urlpatterns',
+    'View data': 'view/',
+    'View specific data': 'view/<str:name>',
+    'Add data': 'add/',
+    'Update data': 'update/<str:name>',
+    'Delete data': 'delete/<str:name>',
+    }
+    return Response(urlpatterns)
 
 @api_view(['GET'])
 def getData(request):
@@ -13,6 +28,14 @@ def getData(request):
     serializer = PersonSerializer(persons, many=True)
     
     return Response(serializer.data)
+
+@api_view(['GET'])
+def getSpecificData(request, name):
+    person = Person.objects.get(name=name)
+    serializer = PersonSerializer(person)
+    
+    return Response(serializer.data)
+    
 
 @api_view(['POST'])
 def addData(request):
@@ -25,12 +48,18 @@ def addData(request):
     #alldata = Person.objects.all()
     #alldataserializer = PersonSerializer(alldata, many=True)
     #return Response(alldataserializer.data)
-
-'''
-@api_view(['DELETE'])
-def deleteData(request):
-    person = Person.objects.get(name=request.data)
-    serializer = PersonSerializer(data=person)
-    person.delete()
+    
+@api_view(['POST'])
+def updateData(request, name):
+    person = Person.objects.get(name=name)
+    serializer = PersonSerializer(instance=person, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
     return Response(serializer.data)
-'''
+
+
+@api_view(['DELETE'])
+def deleteData(request, name):
+    person = Person.objects.get(name=name)
+    person.delete()
+    return Response('Item Deleted')
