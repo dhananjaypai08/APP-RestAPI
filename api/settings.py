c1a71bbf769b9352e51b1e5ffb87882cf39aabc0
dhananjaypai08 <dhananjay2002pai@gmail.com>
2023-05-02 01:22:02 +0530

    setup

--- api/settings.py
+++ api/settings.py
@@ -132,8 +132,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-if config('environ') == 'prod':
-    SECURE_SSL_REDIRECT = True
+# if config('environ') == 'prod':
+#     SECURE_SSL_REDIRECT = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
